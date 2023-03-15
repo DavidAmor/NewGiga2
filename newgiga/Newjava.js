9f7547ce2fc242249f231752499649b4feb6f8bf
SaraTuset <s.tuset.2020@alumnos.urjc.es>
2023-03-15 23:20:14 +0100

    Los subelementos no tienen que ser comentarios

--- newgiga/Newjava.js
+++ newgiga/Newjava.js
@@ -1,19 +1,24 @@
 class comentario {
     texto = new String;
-    //valor = new String;
+    valor = new String;
 
-    constructor( texto){
-        this.texto=texto;
-        //this.valor=valor;
+    constructor(valor, texto){
+       
+        this.valor = valor;
+        this.texto = texto;
     }
 
     getTexto(){
         return this.texto;
     }
 
-   // getValor(){
-   //     return this.valor;
-   // }
+    getValor(){
+        return this.valor;
+    }
+
+    toString(){
+        return this.valor + ": " + this.texto;
+    }
 }
 
 
@@ -29,8 +34,8 @@ class videojuego{
         this.descripcion=descripcion;
     }
 
-    AddComentario(texto){
-        let coment = new comentario(texto);
+    AddComentario(valor, texto){
+        let coment = new comentario(valor, texto);
         this.comentarios.push(coment);
     }
  
@@ -59,23 +64,23 @@ class videojuego{
 
 
 let videojuego1= new videojuego('Super Mario Galaxy','19.99','Embarcate en una aventura de proporciones galácticas, con nuevos potenciadores, planetas de los más rocambolescos y una nueva historia junto a Mario, con una única misión : Salvar el universo.');
-videojuego1.AddComentario("Género : Aventura/Plataformas");
-videojuego1.AddComentario("Un juego bastante antiguo, con unos gráficos bastante estándares, pero con una jugabilidad atrayente e impecable.Si eres fan de Mario, este juego no puede faltar en tu colección.");
+videojuego1.AddComentario("Género :", " Aventura/Plataformas");
+videojuego1.AddComentario("Comentario:", "Un juego bastante antiguo, con unos gráficos bastante estándares, pero con una jugabilidad atrayente e impecable.Si eres fan de Mario, este juego no puede faltar en tu colección.");
 videojuego1.imgJuego.src = "img//12.jpg";
  
-let videojuego2= new videojuego('FIFA 23','69,99','La mayor franquicia de deportes esta de vuelta, experimenta la emoción del fútbol como jugador, o forma el equipo de tus sueños como mánager, y lleva a tu equipo a la victoria.');
-videojuego2.AddComentario("Género : Deportes/Simulación");
-videojuego2.AddComentario("Es lo mismo de siempre, las modificaciones son mínimas, llena de micropagos, solo EA es capaz de hacer algo tan mal.");
+let videojuego2= new videojuego('FIFA 23','69.99','La mayor franquicia de deportes esta de vuelta, experimenta la emoción del fútbol como jugador, o forma el equipo de tus sueños como mánager, y lleva a tu equipo a la victoria.');
+videojuego2.AddComentario("Género :", " Deportes/Simulación");
+videojuego2.AddComentario("Comentario:","Es lo mismo de siempre, las modificaciones son mínimas, llena de micropagos, solo EA es capaz de hacer algo tan mal.");
 videojuego2.imgJuego.src = "img//FIFA.jpg";
 
 let videojuego3= new videojuego('Crash Bandicoot N.Sane Trilogy','24.99','¡3 juegos en uno!Crash esta de vuelta, listo para frustrar los planes del Dr. Neo Cortex y destrozar cajas y enemigos a tráves de 3 aventuras distintas en un solo juego.');
-videojuego3.AddComentario("Género : Plataformas");
-videojuego3.AddComentario("Un regreso triunfal de Crash, con una vuelta a los clasicos remasterizados, es un golpe de nostalgia y al mismo tiempo un soplo de aire fresco.");
+videojuego3.AddComentario("Género :", " Plataformas");
+videojuego3.AddComentario("Comentario:","Un regreso triunfal de Crash, con una vuelta a los clasicos remasterizados, es un golpe de nostalgia y al mismo tiempo un soplo de aire fresco.");
 videojuego3.imgJuego.src = "img//crash.jpeg";
 
 let videojuego4= new videojuego('Modern Warfare 2','19.99','Experimenta la adrenalina del campo del batalla.Acribilla a tus enemigos de cerca o se más clínico con un francotirador, el único objetivo es acabar con el equipo rival y salir victorioso en distintos mapas.O desentyraña la conspiración para acabar con los EE.UU. en el modo historia para un solo jugador.');
-videojuego4.AddComentario("Género : FPS/Acción");
-videojuego4.AddComentario("Tiro al plato y mucha pirotecnia,acción sin pausa y tiros a mansalva.Aunque con una jugabilidad algo rígida y gráficos desfasados, pero dentro de todo disfrutable. ")
+videojuego4.AddComentario("Género :", " FPS/Acción");
+videojuego4.AddComentario("Comentario:","Tiro al plato y mucha pirotecnia,acción sin pausa y tiros a mansalva.Aunque con una jugabilidad algo rígida y gráficos desfasados, pero dentro de todo disfrutable. ")
 videojuego4.imgJuego.src = "img//MW2.jpg";
 
 let videojuegos = [videojuego1, videojuego2,videojuego3,videojuego4];
@@ -179,11 +184,13 @@ function changeComentarios(id){
     console.log(videojuegos[id].comentarios.length);
     for (let i = 0; i < videojuegos[id].comentarios.length+1; i++) {
         let comentarioAInsertar = document.getElementById('newComentario'+ i);
-        if (comentarioAInsertar !== null){
+        let valorAInsertar = document.getElementById('newValor'+ i);
+        if ((comentarioAInsertar !== null)&&(valorAInsertar !== null)){
             let comentarioReal = comentarioAInsertar.value;
+            let valorReal = valorAInsertar.value;
             console.log (comentarioReal);
-            if ((comentarioReal !== " ")&&(comentarioReal !== "")){
-                videojuegos[id].AddComentario(comentarioReal);
+            if ((comentarioReal !== " ")&&(comentarioReal !== "")&&(valorReal !== " ")&&(valorReal !== "")){
+                videojuegos[id].AddComentario(valorReal, comentarioReal);
             }
         }
     }
@@ -262,7 +269,13 @@ function formularioComentarios(coment,id,i){
     let inicio = document.getElementById('div-inicial');
     let divComentario = document.createElement("div");
     inicio.appendChild(divComentario);
-    divComentario.textContent="Comentario "+i+": ";
+    //divComentario.textContent="Comentario "+i+": ";
+
+    let changedValor = document.createElement("input");
+    divComentario.appendChild(changedValor);
+    changedValor.value = coment.getValor();
+    changedValor.id = 'newValor'+ i;
+    console.log(changedValor.id);
 
     let changedComentario = document.createElement("input");
     divComentario.appendChild(changedComentario);
@@ -500,7 +513,7 @@ function addJuegoToDOM(game, i) {
     game.comentarios.forEach(cometario =>{ 
       let pComentarios = document.createElement("p"); 
       divComentarios.appendChild(pComentarios); 
-      pComentarios.textContent = cometario.getTexto();  
+      pComentarios.textContent = cometario.getValor()+cometario.getTexto();  
     });
 
     let volver = document.createElement("button");
@@ -539,12 +552,14 @@ function create(){
     i=0;
     while(document.getElementById('newComentario'+ i)!=null){
         let comentarioAInsertar = document.getElementById('newComentario'+ i).value;
-        newGame.AddComentario(comentarioAInsertar);
+        let ValorAInsertar = document.getElementById('newValor'+ i).value;
+        newGame.AddComentario(ValorAInsertar,comentarioAInsertar);
         i=i+1;
     } 
     let ImgNew = document.getElementById("img_input"); 
     newGame.imgJuego.src = ImgNew.value;
     videojuegos.push(newGame);
+    changeVideojuego(videojuegos.length - 1);
      if ((precioReal !== " ")&&(tituloReal !== " ")&&(isNaN(precioReal) === false)){  //
         return true;
    // videojuegos[videojuegos.length].toString;
